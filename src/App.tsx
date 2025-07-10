3e436822cc8c533b8f8b1d04eee5520b1fc2abc3
Wealthometer <wealthhajoh87@gmail.com>
2025-07-10 23:27:36 +0100

    Update........

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css'
+import './App.css';
+import { Button } from "./components/ui/button"
 
 function App() {
-  return (<div>Hello</div>)
+  return (
+    <div>
+      Hello
+      <Button>Suscribe</Button>
+    </div>
+  )
 }
 
 export default App
