8863b623390ea52fa27b0ccd76d51614316c8cce
Wealthometer <wealthhajoh87@gmail.com>
2025-07-12 22:05:10 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <div>
       ELITE
+      <Button variant="destructive">Susscribe</Button>
     </div>
   )
 }
