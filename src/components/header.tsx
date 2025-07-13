4f7371a42ef20f6816dd0b8596cb35e407087771
Wealthometer <wealthhajoh87@gmail.com>
2025-07-13 19:13:58 +0100

    Update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,22 @@
+import { useTheme } from "../context/theme-provider";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
+  const {theme, setTheme} = useTheme();
+  const isDark = theme === "dark";
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto flex h-16 items-center justify-between px-4">
             <Link to={'/'}>
-                <img src="/logos.png" alt="logo" className="h-14" />
+                <img src={ isDark ? "/logo.png" : "/logo2.png"} alt="logo" className="h-16" />
             </Link>
 
             <div>
                 {/* {Search} */}
                 {/* {Theme toggle} */}
+                <div><Button onClick={() => setTheme( isDark ? "light" : "dark")} variant="ghost">Toggle</Button></div>
             </div>
         </div>
     </header>
