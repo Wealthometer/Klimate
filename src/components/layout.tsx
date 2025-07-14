a52dc7baf31d158d01582e00d3c85ffc6f3161de
Wealthometer <wealthhajoh87@gmail.com>
2025-07-14 11:05:22 +0100

    Update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,6 @@ import { type PropsWithChildren } from 'react'
 const layout = ({children} : PropsWithChildren) => {
   return (
     <div className='bg-gradient-to-br from-background to-muted'>
-        header
          <main className='min-h-screen container mx-auto px-4 py-8'>
             {children}
          </main>
