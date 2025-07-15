feb877148528d245340420d8d7bbbb5a8a12b738
Wealthometer <wealthhajoh87@gmail.com>
2025-07-15 17:39:42 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,26 @@ import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/weather-dashboard'
 import CityPage from './pages/city-page'
 import Header from './components/header'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 // import { Layout } from 'lu'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-      <ThemeProvider defaultTheme='dark'>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<WeatherDashboard />}/>
-          <Route path='/city/:cityName' element={<CityPage />}/>
-        </Routes>
-      </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+       <BrowserRouter>
+        <Header/>
+          <ThemeProvider defaultTheme='dark'>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />}/>
+              <Route path='/city/:cityName' element={<CityPage />}/>
+            </Routes>
+          </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
