1cb9746992fd23a678151152f051caa4612cdb4f
Wealthometer <wealthhajoh87@gmail.com>
2025-07-17 14:46:32 +0100

    Update

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -46,7 +46,7 @@ export function CitySearch() {
     <>
       <Button
         variant="outline"
-        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64 margin-right-1"
         onClick={() => setOpen(true)}
       >
         <Search className="mr-2 h-4 w-4" />
